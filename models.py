1b8a2d93e7538083c764928e3486dffa24f580bf
niraj <neerajmishra55435@gmail.com>
2026-04-06 15:20:29 +0530

    Added Customer,Seller and Product Table to models.py

--- models.py
+++ models.py
@@ -1,6 +1,42 @@
 from database import Base
-from sqlalchemy import Column
+from sqlalchemy import Column, Integer, Boolean, String, ForeignKey, DateTime, Text, Numeric
 
-class Users(Base):
-    __tablename__ = 'users'
+
+class Customer(Base):
+    __tablename__ = 'customer'
+
+    id = Column(Integer,primary_key=True,index=True)
+    email = Column(String , unique=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    hashed_password = Column(String)
+    phone_number = Column(String)
+    address = Column(String)
+    is_active = Column(Boolean ,default=True)
+
+class Seller(Base):
+    __tablename__ = 'Seller'
+
+    seller_id = Column(Integer,primary_key=True,index=True)
+    email = Column(String , unique=True)
+    user_name = Column(String , unique=True)
+    first_name = Column(String)
+    hashed_password = Column(String)
+    phone_number = Column(String)
+    address = Column(String)
+    created_at = Column(DateTime)
+    Bio = Column(Text)
+
+class Product(Base):
+    __tablename__ = 'products'
+    prod_id = Column(Integer , primary_key=True, index=True)
+    name = Column(String)
+    price = Column(Numeric(10,2) , nullable=False)
+    stock = Column(Integer)
+    seller_id = Column(Integer , ForeignKey('Seller.id') , unique=True)
+    category = Column(String)
+
+
+
+
 
