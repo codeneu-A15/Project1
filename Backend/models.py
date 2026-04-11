8749ea17f9455b98f1c6a5b520e27f9ca081c213
niraj <neerajmishra55435@gmail.com>
2026-04-11 15:34:48 +0530

    changed Customer table

--- Backend/models.py
+++ Backend/models.py
@@ -7,8 +7,7 @@ class Customer(Base):
 
     id = Column(Integer,primary_key=True,index=True)
     email = Column(String , unique=True)
-    first_name = Column(String)
-    last_name = Column(String)
+    username = Column(String)
     hashed_password = Column(String)
     phone_number = Column(String)
     address = Column(String)
